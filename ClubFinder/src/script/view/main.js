fb3d2ef8585208ed8397f84204cdb18bbb92a0e3
maulanairfanf <maulanairfanf@gmail.com>
2020-06-10 20:48:27 +0700

    appbar

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,12 +1,12 @@
+import '../component/search-bar.js';
 import DataSource from '../data/data-source.js';
 
 const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
+    const searchElement = document.querySelector("search-bar");
     const clubListElement = document.querySelector("#clubList");
 
     const onButtonSearchClicked = async () => {
-        try{
+        try {
             const result = await DataSource.searchClub(searchElement.value);
             renderResult(result);
         } catch (message) {
@@ -14,7 +14,7 @@ const main = () => {
         }
     };
 
-        const renderResult = results => {
+    const renderResult = results => {
         clubListElement.innerHTML = "";
         results.forEach(club => {
             const {
@@ -22,26 +22,26 @@ const main = () => {
                 fanArt,
                 description
             } = club;
-
             const clubElement = document.createElement("div");
             clubElement.setAttribute("class", "club");
 
             clubElement.innerHTML = `
-            <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-            <div class="club-info">
-                <h2>${name}</h2>
-                <p>${description}</p>
-            </div>`;
+               <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+               <div class="club-info">
+                   <h2>${name}</h2>
+                   <p>${description}</p>
+               </div>`;
+
             clubListElement.appendChild(clubElement);
         })
     };
 
     const fallbackResult = message => {
         clubListElement.innerHTML = "";
-        clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
+        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
     };
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+    searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
